21b1da035723e03276b24e163dc6cfd56c7fde3c
Nurkyz Mamatnazar <nurkyzmamatnazar@gmail.com>
2022-11-25 18:59:46 +0600

    Выходит вся информация по кликанной стране. Устранила проблемы, но если у страны нет границы то ничего не выходит.

--- src/components/Info/Info.tsx
+++ src/components/Info/Info.tsx
@@ -12,57 +12,42 @@ const code_URL = "https://restcountries.com/v2/alpha/";
 const Info: React.FC<Props> = ({clickedCountry}) => {
   const [country, setCountry] = useState<APICountryCode | null>(null);
 
-  let bordersTSX;
-
   const fetchData = useCallback(async (code: string) => {
     const response = await axios.get<APICountryCode>(code_URL + code);
     const countryD = response.data;
 
     const bordersCountry = response.data.borders;
+    const borders: AllCountries[] = [];
+    for (const border of bordersCountry) {
+      const borderCountryResponse = await axios.get<AllCountries>(code_URL + border);
+      const borderCountry = {
+        name: borderCountryResponse.data.name,
+        alpha3Code: borderCountryResponse.data.alpha3Code
+      };
+      borders.push(borderCountry);
+    }
+    const countryInfo: APICountryCode = {
+      name: countryD.name,
+      capital: countryD.capital,
+      flag: countryD.flag,
+      population: countryD.population,
+      borders: borders,
+    };
+    setCountry(countryInfo);
+    }, []);
 
-     if (bordersCountry) {
-       const borders: AllCountries[] = [];
-       for (const border of bordersCountry) {
-         const borderCountryResponse = await axios.get<AllCountries>(code_URL + border);
-         const borderCountry = {
-           name: borderCountryResponse.data.name,
-           alpha3Code: borderCountryResponse.data.alpha3Code
-         };
-         borders.push(borderCountry);
-       }
-       const countryInfo: APICountryCode = {
-         name: countryD.name,
-         capital: countryD.capital,
-         flag: countryD.flag,
-         population: countryD.population,
-         borders: borders,
-       };
-       setCountry(countryInfo);
-       bordersTSX = (
-         <div>
-           <h5>Borders with: </h5>
-           <ol>
-             {country?.borders.map(country => (
-               <li key={country.alpha3Code}>{country.name}</li>
-             ))}
-           </ol>
-         </div>
-       );
-     } else {
-       const borders: AllCountries[] = [];
-       const countryInfo = {
-         name: countryD.name,
-         capital: countryD.capital,
-         flag: countryD.flag,
-         population: countryD.population,
-         borders: borders,
-       };
-       setCountry(countryInfo);
-       bordersTSX = (
-         <div>Borders no!</div>
-       );
-     }
-     }, []);
+  // const borders: AllCountries[] = [];
+  // const countryInfo = {
+  //   name: countryD.name,
+  //   capital: countryD.capital,
+  //   flag: countryD.flag,
+  //   population: countryD.population,
+  //   borders: borders,
+  // };
+  // setCountry(countryInfo);
+  // bordersTSX = (
+  //   <div>Borders no!</div>
+  // );
 
 
    useEffect(() => {
@@ -80,7 +65,14 @@ const Info: React.FC<Props> = ({clickedCountry}) => {
         <p>Capital : {country?.capital} </p>
         <p>Population: {country?.population} </p>
         <img className="image" src={country?.flag} alt={country?.name}/>
-        {bordersTSX}
+        <div>
+          <h5>Borders with: </h5>
+          <ol>
+            {country?.borders.map(country => (
+              <li key={country.alpha3Code}>{country.name}</li>
+            ))}
+          </ol>
+        </div>
       </div>);
     } else {
       info = (
